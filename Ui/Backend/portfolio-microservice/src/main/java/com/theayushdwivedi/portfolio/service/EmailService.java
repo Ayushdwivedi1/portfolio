5fc62aea22aa9244bc51c167d353ce1ac1d494a2
Ayush Dwivedi <100584655+Ayushdwivedi1@users.noreply.github.com>
2025-07-29 14:50:53 +0530

    Delete Ui directory

--- Ui/Backend/portfolio-microservice/src/main/java/com/theayushdwivedi/portfolio/service/EmailService.java
+++
@@ -1,8 +0,0 @@
-package com.theayushdwivedi.portfolio.service;
-
-public interface EmailService {
-    
-    void sendOtpEmail(String to, String otp);
-    
-    void sendPasswordResetEmail(String to, String resetLink);
-} 
